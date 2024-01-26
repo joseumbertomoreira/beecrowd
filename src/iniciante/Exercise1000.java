2c3d95118446f79caf35e597faea72a51b6412e0
stark <stark@localhost.localdomain>
2024-01-26 14:53:47 -0300

    Exercise 1000 and 1001

---
+++ src/iniciante/Exercise1000.java
@@ -0,0 +1,11 @@
+package iniciante;
+
+public class Exercise1000 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
---
+++ src/iniciante/Exercise1001.java
@@ -0,0 +1,22 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Exercise1001 {
+
+    public static void main(String[] args) {
+
+       int A = 0, B = 0, X = 0;
+       Scanner leitura = new Scanner(System.in);
+
+       A = leitura.nextInt();
+       B = leitura.nextInt();
+
+
+       X = A + B;
+
+       System.out.println("X = " + X);
+
+    }
+
+}
